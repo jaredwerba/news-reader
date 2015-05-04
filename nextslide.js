69cabb342824807e662d08c99d4dc586265cb8a7
jaredwerba <jwerba@gmail.com>
2015-05-03 21:08:56 -0400

    trying to animate dots

--- nextslide.js
+++ nextslide.js
@@ -1,11 +1,22 @@
-$('.arrow-next').click(function() {
-  var currentSlide = $('.active-slide');
-  var nextSlide = currentSlide.next();
+var main = function(){
+    $('.dropdown-toggle').click(function(){
+        ('.dropdown-menu').toggle();
+    });
+    //end of click function
+    $('.arrow-next').click(function(){
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+        if (nextSlide.length == 0){
+            nextSlide = $('.slide').first();
+        };
+        //loop
+        $('.active-dot') = currentDot;
+        currentDot.next() = nextDot;
+        $('.active-dot').removeClass('currentDot')
+        $('.active-dot').addClass('nextDot')
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+    });
+};
 
-  if(nextSlide.length == 0) {
-    nextSlide = $('.slide').first();
-  }
-
-  currentSlide.fadeOut(600).removeClass('active-slide');
-  nextSlide.fadeIn(600).addClass('active-slide');
-});
+$(document).ready(main);
