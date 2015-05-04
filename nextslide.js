e6fcd8fc6f5f78a3cabb355901f7506acdf9060f
jaredwerba <jwerba@gmail.com>
2015-05-04 15:26:43 -0400

    needed to define variables before the loop

--- nextslide.js
+++ nextslide.js
@@ -6,16 +6,20 @@ var main = function(){
     $('.arrow-next').click(function(){
         var currentSlide = $('.active-slide');
         var nextSlide = currentSlide.next();
-        if (nextSlide.length == 0){
+        
+        var currentDot = $('.active-dot');
+        var nextDot = currentDot.next();
+        
+            if (nextSlide.length == 0){
             nextSlide = $('.slide').first();
         };
-        //loop
-        $('.active-dot') = currentDot;
-        currentDot.next() = nextDot;
-        $('.active-dot').removeClass('currentDot')
-        $('.active-dot').addClass('nextDot')
+        
+        
         currentSlide.fadeOut(600).removeClass('active-slide');
         nextSlide.fadeIn(600).addClass('active-slide');
+        
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
     });
 };
 
